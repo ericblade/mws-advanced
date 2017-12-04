91ed16efbcb3790aba2b678cb5a49d16aad381d9
Eric Blade <blade.eric@gmail.com>
2017-12-04 17:43:36 -0500

    support GetLowestOfferListingsForASIN / GetLowestPricedOffersForASIN
    - due to bug in MWS services, mws-simple needs to be updated to deal
      with issues, therefore GetLowestPricedOffersForASIN does not yet work
    - add samples for them

---
+++ samples/getLowestOfferListings.js
@@ -0,0 +1,18 @@
+const mws = require('..');
+const keys = require('../test/keys.json');
+
+mws.init(keys);
+
+async function main() {
+    try {
+        const results = await mws.callEndpoint('GetLowestOfferListingsForASIN', {
+            MarketplaceId: 'ATVPDKIKX0DER',
+            ASINList: ['142210284X', '1844161668', '0989391108', 'B009NOF57C'],
+        });
+        console.log(JSON.stringify(results, null, 4));
+    } catch (err) {
+        console.warn('* error', err);
+    }
+}
+
+main();
---
+++ samples/getLowestPricedOffers.js
@@ -0,0 +1,23 @@
+const mws = require('..');
+const keys = require('../test/keys.json');
+
+mws.init(keys);
+
+async function main() {
+    try {
+        const ASINList = ['142210284X', '1844161668', '0989391108', 'B009NOF57C'];
+        ASINList.forEach(async (asin) => {
+            console.warn('asin=', asin);
+            const results = await mws.callEndpoint('GetLowestPricedOffersForASIN', {
+                MarketplaceId: 'ATVPDKIKX0DER',
+                ASIN: asin,
+                ItemCondition: 'New',
+            });
+            console.log(JSON.stringify(results, null, 4));
+        });
+    } catch (err) {
+        console.warn('* error', err);
+    }
+}
+
+main();
