d6a2f1edeebe012a2c23c57bb11b2837ba82349a
Eric Blade <blade.eric@gmail.com>
2018-02-28 12:06:01 -0500

    fix getLowestPricedOffers offer handling when 0 results returned

--- samples/getLowestPricedOffers.js
+++ samples/getLowestPricedOffers.js
@@ -6,7 +6,8 @@ mws.init(keys);
 async function main() {
     try {
         // const ASINList = ['B010YSIKKY']; // '1844161668', '0989391108', 'B009NOF57C'
-        const ASINList = ['B00005V3EZ', 'B010YSIKKY', 'B00IDD9TU8'];
+        // const ASINList = ['B00005V3EZ', 'B010YSIKKY', 'B00IDD9TU8'];
+        const ASINList = ['B00IDD9TU8', 'B00IH00CN0', 'B00WHCK496', 'B00H7LLKJA', 'B00OMCM8W0', 'B00IH057XA', 'B00IQY12WM', 'B00IQY12WM', 'B00NXLNHXK', 'B00IT5R23G', 'B00PR3KZWS'];
         ASINList.forEach(async (asin) => {
             console.warn('asin=', asin);
             const results = await mws.getLowestPricedOffersForASIN({
