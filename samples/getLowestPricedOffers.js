0dc4740a5369ff1b7fdb44dfc5f978b2928817e7
Eric Blade <blade.eric@gmail.com>
2018-02-28 10:47:15 -0500

    fix getLowestPricedOffers not returning useful buy box info

--- samples/getLowestPricedOffers.js
+++ samples/getLowestPricedOffers.js
@@ -5,7 +5,8 @@ mws.init(keys);
 
 async function main() {
     try {
-        const ASINList = ['B010YSIKKY']; // '1844161668', '0989391108', 'B009NOF57C'
+        // const ASINList = ['B010YSIKKY']; // '1844161668', '0989391108', 'B009NOF57C'
+        const ASINList = ['B00005V3EZ', 'B010YSIKKY', 'B00IDD9TU8'];
         ASINList.forEach(async (asin) => {
             console.warn('asin=', asin);
             const results = await mws.getLowestPricedOffersForASIN({
