64d1f415648aeb9bcf8efe6f5a9f4173dde0ac9d
Eric Blade <blade.eric@gmail.com>
2018-03-04 16:08:12 -0500

    fix getLowestPricedOffers for multiple buy-boxes
    - should correctly return an array of all buy-boxes that are active

--- samples/getLowestOfferListings.js
+++ samples/getLowestOfferListings.js
@@ -7,7 +7,8 @@ async function main() {
     try {
         const results = await mws.callEndpoint('GetLowestOfferListingsForASIN', {
             MarketplaceId: 'ATVPDKIKX0DER',
-            ASINList: ['142210284X', '1844161668', '0989391108', 'B009NOF57C'],
+            // ASINList: ['142210284X', '1844161668', '0989391108', 'B009NOF57C'],
+            ASINList: ['B0014QABX0'],
         });
         console.log(JSON.stringify(results, null, 4));
     } catch (err) {
