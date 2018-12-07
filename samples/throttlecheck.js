ea44846aceb7130693c6c20b96901a43a4578d69
Eric Blade <blade.eric@gmail.com>
2018-12-06 21:10:05 -0500

    simple waiting queue for requests
    this should fix a lot of issues, as well as make it a ton easier to build
    systems that won't blow up your api quotas
    
    right now, it's just a simple "wait" queue, it needs to be implemented
    in such a way so that there's a "waiting" and a "running" list, because
    right now it's a hybrid of the old brute-force "re-run everything as soon
    as a timer expires", and a better queue implementation.
    
    throttlecheck has been updated to stagger it's requests, and uses
    getMarketplaces rather than getProductCategories.

--- samples/throttlecheck.js
+++ samples/throttlecheck.js
@@ -1,46 +1,62 @@
+// note that if your throttle is clear at the start of running this, you
+// should expect it to take right around 7 minutes or so to complete a 20
+// request spam to getMarketplaces()
+
 const mws = require('..');
 const keys = require('../test/keys.json');
 
 mws.init(keys);
 
-const getCats = asins => (
-    mws.getProductCategoriesForAsins({
-        marketplaceId: 'ATVPDKIKX0DER',
-        asins,
-    })
-);
+// const getCats = asins => (
+//     console.warn('* getting categories for asin count', asins.length),
+//     mws.getProductCategoriesForAsins({
+//         marketplaceId: 'ATVPDKIKX0DER',
+//         asins,
+//     })
+// );
+
+// async function main() {
+//     return getCats([
+//         '0534645577',
+//         'B00IDD9TU8',
+//         'B00IH00CN0',
+//         'B07CBM6CDD',
+//         'B00OPXKUDK',
+//         '1573833908',
+//         '1578262666',
+//         '517029252X',
+//         '0810408813',
+//         '0793802946',
+//         '0814471048',
+//         'B000GCWNPW',
+//         'B0027DPF62',
+//         'B00005T3EN',
+//         'B00006I5V6',
+//         '159116060X',
+//         'B003FBG4SS',
+//         '0321568095',
+//         'B00006B8FZ',
+//         'B0000AHOOM',
+//         'B00000JQQH',
+//         '0078034639',
+//     ]);
+// }
 
 async function main() {
-    return getCats([
-        '0534645577',
-        'B00IDD9TU8',
-        'B00IH00CN0',
-        'B07CBM6CDD',
-        'B00OPXKUDK',
-        '1573833908',
-        '1578262666',
-        '517029252X',
-        '0810408813',
-        '0793802946',
-        '0814471048',
-        'B000GCWNPW',
-        'B0027DPF62',
-        'B00005T3EN',
-        'B00006I5V6',
-        '159116060X',
-        'B003FBG4SS',
-        '0321568095',
-        'B00006B8FZ',
-        'B0000AHOOM',
-        'B00000JQQH',
-        '0078034639',
-    ]);
+    const arr = [];
+    for (let x = 0; x < 20; x += 1) {
+        arr.push(new Promise((resolve) => {
+            setTimeout(() => mws.getMarketplaces().then(res => resolve(res)), x * 200);
+        }));
+    }
+    await Promise.all(arr).catch(err => console.warn('* err', err));
+    return arr;
 }
 
 /* eslint-disable */
 let x = 1;
 while (x-- > 0) {
-    main().then(x => {
-        console.warn('* x=', x);
+    main().then(res => {
+        console.warn('* res length=', res.length);
     });
 }
